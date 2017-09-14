df7c3b5d08bb958d1c1ee9d1735b030ffd82ec64
Andrus Rinde <andrusrinde@users.noreply.github.com>
2017-09-14 10:47:03 +0300

    Lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Andrus Rinde programmeerib veebi
+	</title>
+</head>
+<body>
+	<h1>Andrus Rinde, veebiprogrammeerimine</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<?php
+		echo "Algas PHP õppimine.";
+	?>
+
+
+</body>
+</html>
